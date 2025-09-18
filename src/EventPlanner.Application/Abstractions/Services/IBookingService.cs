0a86411f0583508b770164d9e16abda403d9e9c8
zhurakovskiyruslan <zhurakovskiyruslan@gmail.com>
2025-09-18 14:52:51 +0300

    updating booking service

--- src/EventPlanner.Application/Abstractions/Services/IBookingService.cs
+++ src/EventPlanner.Application/Abstractions/Services/IBookingService.cs
@@ -7,7 +7,7 @@ public interface IBookingService
     Task<Booking> GetById(int id);
     Task<List<BookingDto>> GetAllAsync();
     Task <List<BookingDto>> GetByUserId(int userId);
-    Task<List<Booking>> GetByEventId(int eventId);
+    Task<List<BookingDto>> GetByEventId(int eventId);
     Task <List<BookingDto>> GetActiveBooking();
     Task<Booking?> GetByUserAndTickets(int userId, int ticketId);
     Task<Booking> CreateAsync(Booking booking);
